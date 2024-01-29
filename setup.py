7135f850e388563958bb262349ac02b05627faef
Jan Steimann <jan.steimann@hhu.de>
2024-01-29 11:56:05 +0100

    Add name and version to setup.py and

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from setuptools import setup
 
 setup(
     include_package_data=True,
+    name='Comment Recommendation Framework',
+    version='0.16.1',
     package_data={
         'comment_recommendation_framework': ['docker-compose.*.yml',
                                              'Dockerfile',
