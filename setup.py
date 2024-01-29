0a0c429ddd936c8161878162523ad1e96bcf2fa7
Jan Steimann <jan.steimann@hhu.de>
2024-01-29 14:52:27 +0100

    Include README.md in package description

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 # setup.py
+from pathlib import Path
 
 from setuptools import setup
 
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     include_package_data=True,
-    name='Comment Recommendation Framework',
-    version='0.16.1',
+    name='comment-recommendation-framework',
+    version='0.16.4',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     package_data={
         'comment_recommendation_framework': ['docker-compose.*.yml',
                                              'Dockerfile',
