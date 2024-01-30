fda5e456b92043a63d852a45eef9d2f0105ea9e9
Jan Steimann <jan.steimann@hhu.de>
2024-01-30 10:46:08 +0100

    Update version number to 0.16.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     include_package_data=True,
     name='comment-recommendation-framework',
-    version='0.16.4',
+    version='0.16.5',
     long_description=long_description,
     long_description_content_type='text/markdown',
     package_data={
